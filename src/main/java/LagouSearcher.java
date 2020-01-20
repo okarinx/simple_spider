6bc8b8bc7e24336fad71cd1e414adee76fece978
okarinx <xs74678@hotmail.com>
2020-01-20 12:19:29 +0800

    add next page

--- src/main/java/LagouSearcher.java
+++ src/main/java/LagouSearcher.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 public class LagouSearcher {
     public static void main(String[] args) {
         // 设置 webdriver 路径
@@ -10,13 +12,46 @@ public class LagouSearcher {
 
         // 创建 webdriver
         WebDriver webDriver = new ChromeDriver();
-
         webDriver.get("https://www.lagou.com/zhaopin/Java/?labelWords=label");
 
-        webDriver.findElement(By.xpath("//div[@class=]"))
+        // 关掉弹窗
+        WebElement btnElement = webDriver.findElement(By.xpath("//div[@class='body-box']//div[@class='body-btn']"));
+        btnElement.click();
 
-        WebElement chosenElement = webDriver.findElement(By.xpath("//li[@class='multi-chosen']//span[contains(text(),'工作经验')]"));
-        WebElement optionElement = chosenElement.findElement(By.xpath("../a[contains(text(),'应届毕业生')]"));
+        // 选中元素
+        clickOption(webDriver, "工作经验", "应届毕业生");
+        clickOption(webDriver, "学历要求", "本科");
+        clickOption(webDriver, "融资阶段", "不限");
+        clickOption(webDriver, "公司规模", "不限");
+        clickOption(webDriver, "行业领域", "移动互联网");
+
+        // 解析页面元素
+        extractJobsByPagination(webDriver);
+
+    }
+
+    private static void extractJobsByPagination(WebDriver webDriver) {
+        List<WebElement> jobElements = webDriver.findElements(By.className("con_list_item"));
+        for (WebElement jobElement : jobElements) {
+            WebElement moneyElement = jobElement.findElement(By.className("position")).findElement(By.className("money"));
+            String companyName = jobElement.findElement(By.className("company_name")).getText();
+            System.out.println(companyName + ":" + moneyElement.getText());
+        }
+        WebElement nextPageBtn = webDriver.findElement(By.className("pager_next"));
+        if(!nextPageBtn.getAttribute("class").contains("disabled")){
+            nextPageBtn.click();
+            try {
+                Thread.sleep(1000L);
+            }catch (InterruptedException e){
+            }
+            extractJobsByPagination(webDriver);
+        }
+
+    }
+
+    private static void clickOption(WebDriver webDriver, String choseTitle, String optionTitle) {
+        WebElement chosenElement = webDriver.findElement(By.xpath("//li[@class='multi-chosen']//span[contains(text(),'" + choseTitle + "')]"));
+        WebElement optionElement = chosenElement.findElement(By.xpath("../a[contains(text(),'" + optionTitle + "')]"));
         optionElement.click();
     }
 }
